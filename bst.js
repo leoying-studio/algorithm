a6d3ae7bee9a6d12314939ac3526b2b278dff52b
leoying <934725772@qq.com>
2023-04-13 17:14:52 +0800

    Update bst.js

--- bst.js
+++ bst.js
@@ -20,14 +20,14 @@ BST.prototype = {
             parent = current;
             // 左子树
             if (value < current.value) {
-                // 每次遍历把left作为当前节点
-                current = current.left;
-                // 如果current 已经为null, 说明已经到了根节点了, 这里的目的就是为了一直找到根节点元素和链表操作是一致的
-                if (current === null) {
+                // 如果current.left 已经为null, 说明已经到了根节点了
+                if (current.left === null) {
                     // 这个时候把对当前的节点进行赋值
                    parent.left = node;
                    break;
                 } 
+                // 每次遍历把left作为当前节点
+                current = current.left;
             } else {
                 current = current.right;
                 if (current === null) {
@@ -36,10 +36,19 @@ BST.prototype = {
                 } 
             }
         }
+    },
+    inOrder: function(node) {
+        // 这里通过深度遍历,对左右子树递归,展示排序后的结果
+        if (node !== null) {
+            this.inOrder(node.left);
+            console.log(node.value + " ");
+            this.inOrder(node.right);
+        }
     }
 }
 
 const bst = new BST(25);
 bst.insert(12);
 bst.insert(8);
-console.log(bst);
+bst.insert(35)
+bst.inOrder(bst.root)
